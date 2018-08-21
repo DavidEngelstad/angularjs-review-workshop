4b08d1e723da41696afe8bf8d8b0956dd81cf268
David Engelstad <davidengelstad@gmail.com>
2018-08-21 08:45:07 -0700

    Exercise-2

--- components/app.js
+++ components/app.js
@@ -1,4 +1,11 @@
 angular.module('todo-list', []).component('app', {
+  controller: function() {
+    this.todo = '';
+    this.todos = [];
+    this.addTodo = e => {
+      this.todos.push(this.todo);
+    };
+  },
   template: `
     Hello from Angular
     `
